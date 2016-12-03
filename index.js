7bba7ac4f739268c24f003a8418bfde541abc950
Shubham Naik <shubham.naik10@gmail.com>
2016-12-03 14:51:57 -0800

    updated environmental load

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let args = process.argv.slice(2);
+// load environment manager to perform checks
+const envManager = require("./utilities/environment.js").manager;
+const fs = require("fs");
 
-if(args && args[0] && (args[0] === "live" || args[0] === "dev" || args[0] === "test")){
-  let mode = "development";
+if(fs.existsSync(".env")){
+  // load environment
+  require("dotenv").load();
 
-  if(args[0] === "live"){
-    mode = "production";
-  } else if(args[0] === "test"){
-    mode = "test"; // should old be run via mocha
-  }
+  // check if .env has everything
+  envManager.checkAttributes(process.env, function(){
+    require('./api/runtime.js')(process.env.MODE);
+  });
 
-  require('./api/runtime.js')(mode);
 } else {
-  console.log("Can't start server, please specify mode (dev or live)");
+  throw "Please create a .env file or type the command 'npm run env'";
 }
