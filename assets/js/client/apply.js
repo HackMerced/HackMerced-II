7da59430485812f8d59f8bdc49a824b1a7c27ce6
Shubham Naik <shubham.naik10@gmail.com>
2016-12-03 03:59:49 -0800

    updated apply

--- assets/js/client/apply.js
+++ assets/js/client/apply.js
@@ -92,7 +92,7 @@ $(document).on("click", ".start-my-app", function(e){
 
         error: function(error){
           lockStartApp = false;
-          console.log(error);
+
           if(error.status === 500 || !error.responseJSON){
             inputError.serverFault();
           } else {
@@ -103,11 +103,24 @@ $(document).on("click", ".start-my-app", function(e){
   }
 });
 
+function checkUserApplicationStatus(status){
+  let $appStatus = $("#apply-GettingToKnowYou .apply-completecount");
+  if(status === "accepted"){ // accepted goes in your acceptance
+    $appStatus.text("completed");
+  } else {
+    $appStatus.text(status);
+  }
+}
+
 function startInnerUser(user){
   $(".apply-section-KeepingLoop").css("display", "none");
+  $(".apply-time-section").removeClass("selected");
+  $("#apply-GettingToKnowYou").addClass("selected");
   $(".apply-section-GettingToKnowYou").fadeIn();
 
   $("#apply-KeepingLoop .apply-completecount").text("Logged In");
+
+  checkUserApplicationStatus(user.status);
 }
 
 $(document).on("click", ".option-signThemUp", function(){
@@ -135,4 +148,61 @@ $(document).on("click", ".ifCollege", function(){
 $(document).on("click", ".ifHighSchool", function(){
   $(".isCollege, .isLoneWolf").css("display", "none");
   $(".isHighSchool").css("display", "block");
+});
+
+$(document).on("click", ".apply-option", function(){
+  const $this = $(this);
+  const $parent = $this.parent();
+
+  if(!$this.hasClass("selected")){
+    $parent.find("input").val($this.text()); // sets the hidden value as this data
+    $parent.find(".selected").removeClass("selected");
+    $this.addClass("selected");
+  }
+});
+
+function generateSurvey(){
+  let survey = {};
+  $(".apply-item:visible").each(function(){
+    const $this = $(this);
+    const name = $this.attr('id').replace("input", "").toLowerCase();
+    const value = $this.find("input").val();
+
+    if(name !== "name" && value){
+      survey[name] = value;
+    }
+  });
+
+  return survey;
+}
+
+
+$(document).on("change", ".apply-option input", function(){
+
+  let send = {
+    name:$("#inputName input").val(),
+    survey:generateSurvey()
+  }
+
+  $.ajax({
+    url: "/api/update",
+    type: "POST",
+    contentType:"application/json",
+    dataType:"json",
+    data:JSON.stringify(send),
+    success: function(user){
+      lockStartApp = false;
+      startInnerUser(user);
+    },
+
+    error: function(error){
+      lockStartApp = false;
+
+      if(error.status === 500 || !error.responseJSON){
+        inputError.serverFault();
+      } else {
+        inputError.userFault(error.responseJSON);
+      }
+    }
+  });
 });
